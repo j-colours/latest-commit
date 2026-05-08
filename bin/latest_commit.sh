0089abb0ca786782f1053824b6fb04b5f3a4bf50
j-colours <shrewsburyj2@winthrop.edu>
2026-05-07 20:09:41 -0400

    Latest Commit #2: basic functionality done but may add more features in the future

---
+++ bin/latest_commit.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# latest_commit
+# Author: jcolours
+# Date: 2026-05-07
+# Description: grabs the latest commit from the git repo in the directory
+
+# Function Name: main
+# Purpose: runs log recent commit
+main() {
+  if [ -d ".git" ]; then
+    # run in background to check if commits have been made
+    git log >/dev/null 2>&1
+
+    # or git log &> /dev/null
+
+    # if the recent run in background doesn't
+    # have an exit code of 0 then error
+    if [ $? -ne 0 ]; then
+      echo
+      echo "Error: No commits currently in repo..."
+      echo "       rerun when atleast one commit is made."
+      exit 1 # exit failure
+    fi
+
+    git log | head -n 6
+  else
+    echo
+    echo "Error: Rerun in a git repo (directory)..."
+    exit 1 # exit failure
+  fi
+
+  exit 0 # exit success
+}
+
+main
