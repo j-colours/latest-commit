1144e5181528469d1b90b22504aba1cf5e12a6cf
j-colours <shrewsburyj2@winthrop.edu>
2026-06-02 23:59:23 -0400

    Latest Commit #19: Added update script within lcommit to avoid cloining and reinstalling

--- latest_commit.sh
+++ latest_commit.sh
@@ -8,6 +8,23 @@
 # Function Name: main
 # Purpose: runs log recent commit
 main() {
+
+  if [[ "$1" == "-u" || "$1" == "--update" ]]; then
+    GITHUB="https://github.com/j-colours/latest-commit/archive/refs/heads/main.zip"
+    CODEBERG="https://codeberg.org/j-colours/latest-commit/archive/refs/heads/main.zip"
+
+    curl -sSL $GITHUB -o /tmp/latest-commit.zip || \ 
+    curl -sSL $CODEBERG -o /tmp/latest-commit.zip
+
+    unzip /tmp/latest-commit.zip -d /tmp/latest-commit
+    mv /tmp/latest-commit/latest-commit-main /usr/local/lib/latest-commit
+    rm -rf /tmp/latest-commit.zip /tmp/latest-commit
+    chmod +x /usr/local/lib/latest-commit/latest_commit.sh
+    ln -sf /usr/local/lib/latest-commit/latest_commit.sh /usr/local/bin/lcommit
+
+    exit 0
+  fi
+
   if [ -d ".git" ]; then
     # run in background to check if commits have been made
     git log >/dev/null 2>&1
@@ -18,13 +35,13 @@ main() {
     # have an exit code of 0 then error
     if [ $? -ne 0 ]; then
       echo
-      echo "Error: No commits currently in repo..."
+      echo -e "$'\033[31m'Error$'\033[0m': No commits currently in repo..."
       echo "       rerun when atleast one commit is made."
       exit 1 # exit failure
     fi
 
     echo
-    echo -e "\x1b[33m[$(basename $PWD)]\x1b[0m" # \x1b hex for ESC character (27)
+    echo -e "\033[33m[$(basename $PWD)]\033[0m" # \x1b hex for ESC character (27)
     echo
 
     git log | head -n 6
@@ -32,10 +49,10 @@ main() {
     # echo $PWD | cut -d "/" -f- --> didn't work --> to work must be echo $PWD | rev | cut -d "/" -f1 | rev
     # echo $PWD | awk -F'/' '{print $NF}' --> works!
 
-    echo $'\x1b[33m'"[$(basename $PWD)]"$'\x1b[0m' # \x1b hex for ESC character (27)
+    echo $'\033[33m'"[$(basename $PWD)]"$'\033[0m' # \x1b hex for ESC character (27) or 033
   else
     echo
-    echo "Error: Not in a git repo (directory)..."
+    echo "$'\033[31m'Error$'\033[0m': Not in a git repo (directory)..."
     exit 1 # exit failure
   fi
 
